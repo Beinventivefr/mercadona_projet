b708aaa9877616294e4190ce08eec8ffed1d3523
be inventive <beinventivefr@gmail.com>
2023-05-14 18:44:12 +0200

    installation de django rest framework api

--- backend/Mercadona_projet/catergory/models.py
+++ backend/Mercadona_projet/catergory/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Catergory(models.Model):
-    wording = models.CharField(max_length=50)
+    wording = models.CharField(max_length=45)
 
     class Meta:
         verbose_name = 'Catergory'
--- backend/Mercadona_projet/catergory/tests.py
+++ backend/Mercadona_projet/catergory/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
+from catergory.models import Catergory
+
 # Create your tests here.
+class CatergoryTestCase(TestCase):
+
+    def test_create_category(self):
+
+        # Voir combien d'élément sont présent dans la db
+        nbr_of_catergory_before_add = Catergory.objects.count()
+
+        # Ajouter un objet dans la db
+        new_catergory = Catergory()
+        new_catergory.wording = "Mur"
+        new_catergory.save()
+
+        nbr_of_catergory_after_add = Catergory.objects.count()
+
+        # valider que le nombre d'objet dans la db a été incrémenté de 1
+        self.assertTrue(nbr_of_catergory_after_add == nbr_of_catergory_before_add + 1)
+
