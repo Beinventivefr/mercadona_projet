389a3fd9b7adf3eff46ecd109d1ffa04188b62dd
be inventive <beinventivefr@gmail.com>
2023-05-14 14:05:28 +0200

    creation du models et de admin.py

--- backend/Mercadona_projet/catergory/admin.py
+++ backend/Mercadona_projet/catergory/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from catergory.models import Catergory
+
+
 # Register your models here.
+@admin.register(Catergory)
+class CatergoryAdmin(admin.ModelAdmin):
+
+    list_display = ('wording',)
+
+    # Dans ce cas, nous avons spécifié list_display wording pour dire à Django de nafficher 
+    # que le champ wording lorsque lutilisateur regarde la liste des instances de notre modèle dans linterface dadministration. 
+    # En dautres termes, lorsque lutilisateur navigue vers la page de liste des catégories, il ne verra que la colonne contenant le libellé de chaque catégorie.
+
+
--- backend/Mercadona_projet/catergory/models.py
+++ backend/Mercadona_projet/catergory/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
+class Catergory(models.Model):
     wording = models.CharField(max_length=50)
 
     class Meta:
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
+        verbose_name = 'Catergory'
+        verbose_name_plural = 'Catergories'
+
 
